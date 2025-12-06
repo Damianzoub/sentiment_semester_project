f28a9832ce6ed2352675af30b8a51b89fcb45689
Damianzoub <d.zoumpos04@gmail.com>
2025-12-06 13:47:56 +0200

    changed the preporcess and the pipeline to see what it is better

--- src/preprocess/create_pipeline.py
+++ src/preprocess/create_pipeline.py
@@ -2,35 +2,37 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC 
 from sklearn.neural_network import MLPClassifier
-from metrics.return_metrics import return_metrics,calculate_inference_time,calculate_training_time
-from sklearn.metrics import classification_report
+from preprocess_text import TextCleaner
 
-class PipelineCreation:
-    def __init__(self,model_type:str='mlp',random_state:int=42):
-        self.model_type = model_type
-        self.random_state = random_state
-        self.pipeline = self._build_pipeline()
-    def _build_pipeline(self):
-        if self.model_type == "svm":
-            clf = SVC()
-        elif self.model_type== "mlp":
-            clf = MLPClassifier()
-        else:
-            raise ValueError("Wrong Model Type")
+def build_tfidf_svc():
+    return Pipeline([(
+        "cleaner",TextCleaner(),
+    ),(
+        "tfidf",TfidfVectorizer(
+            ngram_range=(1,2),
+            max_features=40000,
+            stop_words="english"
+        )
+    ),(
+        "clf",SVC()
+    )])
 
-        pipe = Pipeline([("tfidf",TfidfVectorizer(
-            max_features=40000,ngram_range=(1,2),stop_words="english"
-        )),(
-            "clf",clf
-        )])
-
-        return pipe 
-
-    def fit(self,x_train,y_train):
-        self.pipeline.fit(x_train,y_train)
-        return self
-    
-    def evaluate(self,x_test,y_test,verbose=True):
-        pass
-
-    
+def build_tfidf_nn():
+    return Pipeline([(
+        "cleaner",TextCleaner()
+    ),(
+        "tfidf",TfidfVectorizer(
+            ngram_range=(1,2),
+            max_features=40000,
+            stop_words="english"
+        )
+    ),("clf",MLPClassifier(
+        hidden_layer_sizes=(256,128,64),
+        activation='relu',
+        solver='adam',
+        learning_rate='adaptive',
+        alpha=1e-3,
+        batch_size=64,
+        max_iter=50,
+        random_state=42
+    ))])
--- src/preprocess/dataset.py
+++ src/preprocess/dataset.py
@@ -1,27 +1,22 @@
+from pathlib import Path 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from preprocess_text import clean_html_tags
-def train_val_split(df:pd.DataFrame,text_col:str='review',label_col:str='label',test_size=0.2,val_size=0.1,random_state=42):
-    df = df.dropna(subset=[''])
-    df[text_col] = df[text_col].apply(clean_html_tags)
-    if "type" in df.columns:
-        train_df = df[df['split']=='train']
-        test_df = df[df['split']=='test']
-        train_df, val_df = train_test_split(
-            train_df,
-            test_size=val_size,
-            random_state=random_state,
-            stratify=train_df[label_col]
-        )
-    else:
-        train_df,test_df = train_test_split(
-            df,test_size=test_size,random_state=random_state,stratify=df[label_col]
-        )
-        train_df,val_df = train_test_split(
-            train_df,test_size=val_size,random_state=random_state,stratify=train_df[label_col]
-        )
-    
-    return (
-        train_df[text_col],val_df[text_col],test_df[text_col],
-        train_df[label_col],val_df[label_col],test_df[label_col]    
-    )
+import joblib 
+
+TEXT_COL = "review"
+LABEL_COL ="label"
+TEST_SIZE=0.2
+RANDOM_STATE=42
+
+def load_raw():
+    df = pd.read_csv("",encoding='latin-1')
+    df = df.drop(columns=['Unnamed: 0','file']).reset_index(drop=True)
+    #for now i will not focus on nan values because we don't have 
+    return df 
+
+def make_split():
+    pass 
+
+def load_split():
+    #maybe we can put this if we have in ram the data and we take them fast to make the process faster
+    pass
